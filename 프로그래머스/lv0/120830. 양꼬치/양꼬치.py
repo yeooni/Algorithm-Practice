f4d4aaeefb576e781b684a95d83c81a993ce3321
yeooni <97443195+yeooni@users.noreply.github.com>
2023-05-03 16:45:03 +0900

    [level 0] Title: 양꼬치, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120830. 양꼬치/양꼬치.py
@@ -0,0 +1,13 @@
+def solution(n, k):
+    answer = 0
+    
+    price_food = n * 12000
+    
+    if n >= 10:
+        price_drink = (k - n // 10) * 2000
+    else : 
+        price_drink = k * 2000
+    
+    answer = price_food + price_drink
+    
+    return answer
