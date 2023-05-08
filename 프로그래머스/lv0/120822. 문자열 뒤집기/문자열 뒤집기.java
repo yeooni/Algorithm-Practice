2eb2053014c91ee598fb77b98cda96b6780f628f
yeooni <97443195+yeooni@users.noreply.github.com>
2023-05-08 16:41:55 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.04 ms, Memory: 70.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,14 +1,9 @@
 class Solution {
     public String solution(String my_string) {
-        String answer = "";
-        char[] ch = my_string.toCharArray();
-        char[] reverse = new char[ch.length];
+        StringBuilder sb = new StringBuilder();
+        sb.append(my_string);
+        sb.reverse();
         
-        for(int i = 0; i < ch.length; i++){
-            reverse[i] = ch[ch.length -i -1];
-        }
-        answer = new String(reverse);
-        
-        return answer;
+        return sb.toString();
     }
 }
