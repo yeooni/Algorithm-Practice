e427e06772ecb8e27d6981b46b930bed0f3c690a
yeooni <97443195+yeooni@users.noreply.github.com>
2023-05-15 16:30:34 +0900

    [unrated] Title: n 번째 원소까지, Time: 0.02 ms, Memory: 86.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181889. n 번째 원소까지/n 번째 원소까지.java
+++ 프로그래머스/unrated/181889. n 번째 원소까지/n 번째 원소까지.java
@@ -1,10 +1,7 @@
+import java.util.Arrays;
+
 class Solution {
     public int[] solution(int[] num_list, int n) {
-        int[] answer = new int[n];
-        
-        for(int i = 0; i<n; i++){
-            answer[i] = num_list[i];
-        }
-        return answer;
+        return Arrays.copyOfRange(num_list, 0, n);
     }
 }
