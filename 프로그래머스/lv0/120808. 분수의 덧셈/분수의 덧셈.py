8ec12db68cdbdcb8e26e1f15aeefbf72587659b0
yeooni <97443195+yeooni@users.noreply.github.com>
2023-05-26 11:02:26 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.01 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.py
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -1,16 +1,7 @@
-def gcd(a, b):
-    if a % b == 0:
-        return b
-    return gcd(b, a % b)
+# 분모가 다른 분수의 계산을 편하게 해주는 함수     
+from fractions import Fraction
 
 def solution(denum1, num1, denum2, num2):
-    # 1. 두 분수의 합 계산
-    boonmo = num1 * num2
-    boonja = denum1 * num2 + denum2 * num1
-    
-    # 2. 최대공약수 계산
-    gcd_value = gcd(boonmo, boonja)
-
-    # 3. gcd 로 나눈 값을 answer에 담기
-    answer = [boonja / gcd_value, boonmo / gcd_value]
+    f = Fraction(denum1, num1) + Fraction(denum2, num2)
+    answer = [f.numerator, f.denominator]
     return answer
