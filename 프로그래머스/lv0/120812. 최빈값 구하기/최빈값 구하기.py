6cdca42a28000fc9d09c2867c79f12f8019174c0
yeooni <97443195+yeooni@users.noreply.github.com>
2023-06-01 15:58:45 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.02 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.py
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.py
@@ -1,16 +1,7 @@
 def solution(array):
-    count = [0] * (max(array) + 1) #숫자 횟수 세는 리스트
-    
-    for i in array:
-        count[i] += 1
-        
-    m = 0 #최빈값 개수
-    
-    for c in count:
-        if c == max(count):
-            m += 1
-    
-    if m > 1: #최빈값이 2개 이상이면 -1을 리턴
-        return -1
-    else: #최빈값이 1개이면 해당 숫자를 리턴
-        return count.index(max(count))
+    while len(array) != 0:
+        for i, a in enumerate(set(array)):
+            array.remove(a)
+        if i == 0:
+            return a
+    return -1
