b0ccf83bc686e0f3258a7def2147662887d2e2f0
yeooni <97443195+yeooni@users.noreply.github.com>
2023-06-20 16:11:33 +0900

    [unrated] Title: 접두사인지 확인하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.py
+++ 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.py
@@ -1,8 +1,3 @@
 def solution(my_string, is_prefix):
-    answer = 0
-    tmp = [my_string[:i] for i in range(0, len(my_string))]
-    for i in tmp:
-        if is_prefix == i:
-            answer = 1
     
-    return answer
+    return int(my_string.startswith(is_prefix))
