c3fd9c5a37146174758bf46e5b0bb53c3b219eb8
yeooni <97443195+yeooni@users.noreply.github.com>
2023-06-30 14:19:33 +0900

    [unrated] Title: 수 조작하기 1, Time: 1.09 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.java
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.java
@@ -1,23 +1,26 @@
 class Solution {
     public int solution(int n, String control) {
         int answer = n;
-        
-        String[] controlList = control.split("");
-        
-        for(int i = 0; i < controlList.length; i++) {
-            if(controlList[i].equals("w")) {
-                answer += 1;
-            }
-        else if(controlList[i].equals("s")){
-                answer-=1;
-            }
-            else if(controlList[i].equals("d")){
-                answer+=10;
-            }
-            else if(controlList[i].equals("a")){
-                answer-=10;
+
+        for(char ch : control.toCharArray()) {
+            switch(ch) {
+                case 'w': 
+                    answer += 1; 
+                    break;
+                case 's': 
+                    answer -= 1; 
+                    break;
+                case 'd': 
+                    answer += 10; 
+                    break;
+                case 'a': 
+                    answer -= 10; 
+                    break;
+                    
+                default:break;
             }
         }
+
         return answer;
     }
 }
