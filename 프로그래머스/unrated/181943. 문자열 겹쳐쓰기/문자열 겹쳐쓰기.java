39afc949c7a6d0dfee673c4356a8e9a3970dc96c
yeooni <97443195+yeooni@users.noreply.github.com>
2023-07-06 17:16:14 +0900

    [unrated] Title: 문자열 겹쳐쓰기, Time: 18.37 ms, Memory: 86.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String my_string, String overwrite_string, int s) {
+        String answer = "";
+        
+        String str = my_string.substring(0, s);
+        String ostr = my_string.substring(s + overwrite_string.length());
+        answer += str + overwrite_string + ostr;
+        
+        return answer;
+    }
+}
