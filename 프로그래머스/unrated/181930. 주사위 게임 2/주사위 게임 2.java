09fb989409d138b58ce98fa772869a59d8f958e2
yeooni <97443195+yeooni@users.noreply.github.com>
2023-08-14 10:07:40 +0900

    [unrated] Title: 주사위 게임 2, Time: 0.02 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.java
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.java
@@ -1,14 +1,27 @@
 class Solution {
     public int solution(int a, int b, int c) {
-        int answer = 0;
-        if(a==b&&b==c) {
-            answer = (a+b+c)*(a*a+b*b+c*c)*(a*a*a+b*b*b+c*c*c);
-        } else if(a==b&&b!=c||a==c&&c!=b||a!=b&&b==c) {
-            answer = (a+b+c)*(a*a+b*b+c*c);
-        } else {
-            answer = a+b+c;
+        int answer = 1;
+
+        int count = 1;
+        if(a == b || a == c || b == c) {
+            count++;
         }
-        
+
+        if(a == b && b == c) {
+            count++;
+        }
+
+        for(int i = 1; i <= count; i++) {
+            answer *= (pow(a,i)+pow(b,i)+pow(c,i));
+        }
+
         return answer;
+    }
+
+    private int pow(int a, int b) {
+        if(b == 0) {
+            return 1;
+        }
+        return a * pow(a, b-1);
     }
 }
