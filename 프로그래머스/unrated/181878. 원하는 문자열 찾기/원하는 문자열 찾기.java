fac6bc6afe3007c4fde11f443fc695f28ba88432
yeooni <97443195+yeooni@users.noreply.github.com>
2023-08-16 13:54:44 +0900

    [unrated] Title: 원하는 문자열 찾기, Time: 5.43 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(String myString, String pat) {
+        int answer = 0;
+        
+        if(myString.toLowerCase().contains(pat.toLowerCase())) {
+            return answer = 1;
+        } 
+        
+        return answer;
+    }
+}
