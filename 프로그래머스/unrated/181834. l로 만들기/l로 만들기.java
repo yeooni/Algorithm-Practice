8b6a036e27d976246968dd15a0b190e7b2e22af9
yeooni <97443195+yeooni@users.noreply.github.com>
2023-08-23 16:01:36 +0900

    [unrated] Title: l로 만들기, Time: 5.90 ms, Memory: 84 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181834. l로 만들기/l로 만들기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String myString) {
+        StringBuilder sb = new StringBuilder();
+        
+        for(char ch : myString.toCharArray()) {
+            if(ch < 'l') {
+                sb.append('l');
+            } else {
+                sb.append(ch);
+            }
+        }
+        return sb.toString();
+    }
+}
